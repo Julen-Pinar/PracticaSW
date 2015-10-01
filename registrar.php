aec1f6f22cdee363980c2e01387595ba4ba42ac6
Jon Led <snakers.7@gmail.com>
2015-10-01 10:33:58 +0200

    CL:01/10:B Iniciar registrar.php

---
+++ registrar.php
@@ -0,0 +1,17 @@
+<?php
+echo $_POST["Email"] . "<br>";
+    echo $_POST["Nombre"] . "<br>";
+        echo $_POST["Apellido1"] . "<br>";
+                echo $_POST["Apellido2"] . "<br>";
+
+$email = $_POST["Email"];
+$nombre = $_POST["Nombre"];
+$apellido1 = $_POST["Apellido1"];
+$apellido2 = $_POST["Apellido2"];
+$password = $_POST["Password"];
+$telefono = $_POST["Telefono"];
+$especialidad = $_POST["Especialidad"];
+$intereses = $_POST["intereses"];
+
+
+?>
