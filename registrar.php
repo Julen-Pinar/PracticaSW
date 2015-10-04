4bc304dbb69c5958e68e45d75c73819f19d02944
Julen-Pinar <julen.pinar@gmail.com>
2015-10-04 18:40:09 +0200

    registrar.php: Fix Otros

--- registrar.php
+++ registrar.php
@@ -1,8 +1,18 @@
 <?php
 mysql_connect("localhost","root","") or die(mysql_error());
 mysql_select_db("quiz") or die(mysql_error());
+
+$otro= $_POST['Especialidad'];
+
+
+if (strcmp ($otro, "Otros")==0)
+{
+	$otro=$_POST['otraEspecialidad'];
+	echo $_POST['otraEspecialidad'];
+}
+
 $sql="INSERT INTO usuario(email, nombre, primerapellido, segundoapellido, password, telefono, especialidad, intereses) VALUES
-('$_POST[Email]','$_POST[Nombre]','$_POST[Apellido1]','$_POST[Apellido2]','$_POST[Password]','$_POST[Telefono]','$_POST[Especialidad]','$_POST[Intereses]')";
+('$_POST[Email]','$_POST[Nombre]','$_POST[Apellido1]','$_POST[Apellido2]','$_POST[Password]','$_POST[Telefono]', '$otro' ,'$_POST[Intereses]')";
 if (!mysql_query($sql))
 {
 die('Error: ' . mysql_error());
