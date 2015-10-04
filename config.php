6c4d3d3230d624a194fd1e4fb55802bcb99fcad1
Julen-Pinar <julen.pinar@gmail.com>
2015-10-04 19:46:28 +0200

    hotfix config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
   $DOMAIN = "mysql.hostinger.es";
   $USERNAME = "u243202774_sw";
   $PASSWORD = "sistemasweb";
-  $BD = "u243202774_quiz";
+  $DB = "u243202774_quiz";
   mysql_connect($DOMAIN, $USERNAME, $PASSWORD) or die(mysql_error());
   mysql_select_db($DB) or die(mysql_error());
 ?>
