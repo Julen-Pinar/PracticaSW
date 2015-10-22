4b5e7e023a9fb4d333affb622dc0a737573ff078
Julen-Pinar <julen.pinar@gmail.com>
2015-10-22 19:21:03 +0200

    hotfix

--- add_insertarPregunta.php
+++ add_insertarPregunta.php
@@ -4,13 +4,14 @@
   $respuesta = $_POST['Respuesta'];
   $complex = $_POST['Complejidad'];
 
-$sql="INSERT INTO preguntas(pregunta, respuesta, complejidad) VALUES ('$pregunta', '$respuesta','$complex')";
+$sql="INSERT INTO preguntas(pregunta, respuesta, complejidad) VALUES ('$pregunta', '$respuesta',$complex)";
 	
-	if (!mysql_query($sql))
+if (!mysql_query($sql))
 {
 
 die('Error: ' . mysql_error());
 }
-echo "1 question added";
+
+echo "<center>1 question added</center>";
 mysql_close();
 ?>
