bf686f93c5341d1e4746ebe5901f89faff207902
Jon Led <snakers.7@gmail.com>
2015-10-22 18:04:09 +0200

    22/10:Efix Añadido comprobación de sesión en insertarPregunta

--- insertarPregunta.php
+++ insertarPregunta.php
@@ -8,7 +8,7 @@
 </HEAD>
 <BODY>
 	<?php
-		if(!(isset($_SESSION['login']) && $_SESSION['login'] != '') {
+		if (!(isset($_SESSION['login']) && $_SESSION['login'] != '')) {
 			header("Location: login.php");
 		}
 	?>
