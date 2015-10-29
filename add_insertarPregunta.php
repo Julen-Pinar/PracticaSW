98a9ce18cf91370c5f2b1c7c14191b525895af48
Jon Led <snakers.7@gmail.com>
2015-10-29 12:27:03 +0100

    29/10:B Terminada lógica XML de insertarPregunta.php

--- add_insertarPregunta.php
+++ add_insertarPregunta.php
@@ -3,7 +3,7 @@
   $pregunta = $_POST['Pregunta'];
   $respuesta = $_POST['Respuesta'];
   $complex = $_POST['Complejidad'];
-  $subject = $_POST['Tema']
+  $subject = $_POST['Tema'];
 $sql="INSERT INTO preguntas(pregunta, respuesta, complejidad) VALUES ('$pregunta', '$respuesta',$complex)";
 
 if (!mysql_query($sql))
@@ -14,17 +14,24 @@ die('Error: ' . mysql_error());
 
 $xml = simplexml_load_file('preguntas.xml');
 $assessmentItem = $xml->addChild('assessmentItem');
-if(!empty($complex)) {
+//if(!isset($complex)) {
   $assessmentItem->addAttribute('complexity',$complex);
-}
-$assessmentItem->addAttribute('subject', 'dont fucking care');
+//}
+$assessmentItem->addAttribute('subject', $subject);
 $preg= $assessmentItem->addChild('itemBody');
 $preg->addChild('p',$pregunta);
 $res=$assessmentItem->addChild('correctResponse');
 $res->addChild('value', $respuesta);
 
-$xml->asXML('preguntas.xml');
 
-echo "<center>1 question added</center><br>";
+
+echo "<br /><center>1 question added</center><br>";
+
+if(!$xml->asXML('preguntas.xml')) {
+  echo "<center><span class=error>Ha habido un error al guardar el XML</span></center>";
+} else {
+  echo "<center>Introducido correctamente al XML <br /><a href=\"preguntas.xml\">Ver aquí</a> </center><br />";
+}
+
 mysql_close();
 ?>
--- quizes.php
+++ quizes.php
@@ -1,5 +1,5 @@
 <?php
-include("config.php");
+include("configlocal.php");
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <HTML>
