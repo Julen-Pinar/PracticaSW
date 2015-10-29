915c128a9449f34668629992afb14caf61081cd1
Jon Led <snakers.7@gmail.com>
2015-10-29 12:09:58 +0100

    29/10:A Añadida lógica XML de insertarPregunta.php

--- add_insertarPregunta.php
+++ add_insertarPregunta.php
@@ -3,24 +3,26 @@
   $pregunta = $_POST['Pregunta'];
   $respuesta = $_POST['Respuesta'];
   $complex = $_POST['Complejidad'];
-/*
+  $subject = $_POST['Tema']
 $sql="INSERT INTO preguntas(pregunta, respuesta, complejidad) VALUES ('$pregunta', '$respuesta',$complex)";
-	
+
 if (!mysql_query($sql))
 {
 
 die('Error: ' . mysql_error());
 }
-*/
-$xml = simplexml_load_file('preguntas.xml');
-//$ais = $xml->addChild('assessmentItems');
-$ai = $xml->addChild('assessmentItem');
-$preg= $ai->addChild('itemBody');
-$preg->addChild('p',$_POST['Pregunta']);
-$res=$ai->addChild('correctResponse');
-$res->addChild('value', $_POST['Respuesta']);
 
-echo $xml->asXML();
+$xml = simplexml_load_file('preguntas.xml');
+$assessmentItem = $xml->addChild('assessmentItem');
+if(!empty($complex)) {
+  $assessmentItem->addAttribute('complexity',$complex);
+}
+$assessmentItem->addAttribute('subject', 'dont fucking care');
+$preg= $assessmentItem->addChild('itemBody');
+$preg->addChild('p',$pregunta);
+$res=$assessmentItem->addChild('correctResponse');
+$res->addChild('value', $respuesta);
+
 $xml->asXML('preguntas.xml');
 
 echo "<center>1 question added</center><br>";
--- formulario_insertarPregunta.php
+++ formulario_insertarPregunta.php
@@ -2,7 +2,9 @@
 	  <form id="insertar" method="POST" action="" name="insertar" onsubmit="">
 	   <p>  Pregunta: <input name="Pregunta" id="Pregunta" type="text"></p>
 	   <p>  Respuesta: <input name="Respuesta" id="Respuesta" type="text"></p>
+		  <p> Tema: <input name="Tema" id="Tema" type="text"></p>
 	   <p> Complejidad (opcional): <input name="Complejidad" id="Complejidad" type="number" min="1" max="5"></p>
+
 	    <input type=submit>
 		</form>
 	   </DIV>
